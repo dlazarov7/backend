9dce200d88bfb78e6b4bd51713f4c325fba3e7a3
Dimitar Lazarov <lazarovd12@gmail.com>
2023-04-28 11:15:30 +0300

    .env

--- src/data-source.ts
+++ src/data-source.ts
@@ -3,14 +3,16 @@ import { DataSource } from "typeorm"
 import { Employee } from "./models/Employee"
 import { Team } from "./models/Team"
 import { Company} from "./models/Company"
+import * as dotenv from "dotenv";
+dotenv.config()
 
 export const AppDataSource = new DataSource({
     type: "postgres",
-    host: "localhost",
-    port: 5432,
-    username: "postgres",
-    password: "kozel77",
-    database: "company-teams-management",
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     synchronize: false,
     logging: true,
     //cache:true,
