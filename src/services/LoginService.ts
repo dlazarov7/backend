52084f6dc1b8008495d07ea15fcd564d5ba32a5c
Dimitar Lazarov <lazarovd12@gmail.com>
2023-05-17 15:41:25 +0300

    login

--- src/services/LoginService.ts
+++ src/services/LoginService.ts
@@ -1,5 +1,10 @@
 import { AppDataSource } from "../data-source";
 import { Employee } from "../models/Employee";
+import bcrypt from 'bcrypt';
+import { mapper } from "../utilities/mapper/AutoMapper";
+import EmployeeDto from "../dtos/EmployeeDto";
+import EmployeeRegisterDto from "../dtos/EmployeeRegisterDto";
+import { createMap } from "@automapper/core";
 
 export class LoginService {
 
@@ -8,24 +13,32 @@ export class LoginService {
     static getInstance() {
         if (!this.instance) {
             this.instance = new LoginService();
+            createMap(mapper,EmployeeRegisterDto,Employee);
+            createMap(mapper,EmployeeDto,Employee);
+            createMap(mapper,Employee,EmployeeDto);
         }
         return this.instance;
     }
 
-    async userAuthentication(email:string,password:string){
-        const result=await AppDataSource
-        .getRepository(Employee)
-        .createQueryBuilder("emp")
-        .select(`emp.first_name, emp.last_name`)
-        .where('emp.email=:email',{email: email})
-        .andWhere('emp.password=:password',{password:password})
-        .getRawOne()
-        
-        return result;
+    async userAuthentication(userData: EmployeeRegisterDto) {
+
+        const hashedPassword = await bcrypt.hash(userData.password, 10);
+        userData.password = hashedPassword;
+
+        const user=mapper.map(userData,EmployeeRegisterDto,Employee);
+
+        const result = await AppDataSource
+            .getRepository(Employee)
+            .createQueryBuilder("emp")
+            .where('emp.email=:email', { email: user.email })
+            .andWhere('emp.password=:password', { password: user.password })
+            .getOne();
+
+        return mapper.map(result,Employee,EmployeeDto);
     }
 
-    async isUserLogedgout (){
-        
+    async isUserLogedgout() {
+
         return "Successfuly logedout";
     }
 }
